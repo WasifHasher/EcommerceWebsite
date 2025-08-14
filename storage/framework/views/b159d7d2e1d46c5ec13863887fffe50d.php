68c50997e6237b6f3ac29dd992ff362203d1ef01
WasifHasher <157693651+WasifHasher@users.noreply.github.com>
2025-08-14 18:03:10 +0500

    add files

---
+++ storage/framework/views/b159d7d2e1d46c5ec13863887fffe50d.php
@@ -0,0 +1,34 @@
+
+<?php $__env->startSection('contant'); ?>
+
+<div class="container">
+    <h2 class="mt-3">Trending Items</h2>
+      <div class="row">
+        <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+          <div class="col-3">
+           
+            <a href="detail/<?php echo e($item['id']); ?>">
+
+              <img src="<?php echo e(asset($item->gallery)); ?>" height="300px" class="d-block w-100 mt-3" alt="..."> 
+              <div class="py-3">
+                  <div class="d-flex ">
+                        <h5><?php echo e($item['name']); ?></h5> 
+                        <span style="margin-left:150px"><?php echo e($item['category']); ?></span>
+                  </div> 
+                      <p><?php echo e($item['price']); ?></p>
+              </div>
+
+            </a>
+ 
+          </div>
+          <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+      </div>
+    </div>
+    
+ 
+
+
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\LaravelProjects\EcommerceProject\resources\views\search.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f855054c452fb004ad816b371422ab6f.php
@@ -0,0 +1,60 @@
+
+<?php $__env->startSection('contant'); ?>
+
+
+
+
+    <div class="container mt-5" style="height:600px;">
+        <table class="table table-striped">
+            
+            <tbody>
+                <tr>
+                    <td>Price</td>
+                    <td><?php echo e($total); ?></td>
+                </tr>
+                <tr>
+                    <td>Tax</td>
+                    <td>0 PKR</td>
+                </tr>
+                <tr>
+                    <td>Delivery</td>
+                    <td>400 PKR</td>
+                </tr>
+                <tr>
+                    <td>Total</td>
+                    <td><?php echo e($total+400); ?></td>
+                </tr>
+            </tbody>
+        </table>
+
+
+        <form action="/orderplace" method="POST">
+            <?php echo csrf_field(); ?>
+            <div class="mb-3">
+             
+              <textarea name="address" id="" cols="80" rows="5" placeholder="Enter your Address" class="ps-2" ></textarea>
+             
+              
+            </div>
+            <div class="mb-1">
+
+                <label for="" class="fs-3">Payment</label>
+                <hr>
+                <p><input type="radio" name="payment" ><span class="ps-2">Payment</span></p>
+                <p><input type="radio" name="payment" ><span class="ps-2">EMI Payment</span></p>
+                <p><input type="radio" name="payment" ><span class="ps-2">Payment On Delivery</span></p>
+              
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+
+
+    </div>
+ 
+
+
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\LaravelProjects\EcommerceProject\resources\views\orderNow.blade.php ENDPATH**/ ?>
